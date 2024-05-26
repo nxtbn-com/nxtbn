bba9a7136f7b073c9d4f6057ddfdafef17adcef3
MySecondLanguage <bytenyxlimited@gmail.com>
2024-05-26 16:17:50 +0600

    Refresh currency buttion added in admin

--- nxtbn/core/admin.py
+++ nxtbn/core/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import path
@@ -9,11 +10,30 @@ from nxtbn.core.currency.utils import currency_Backend
 admin.site.register(SiteSettings)
 
 
-def refresh_currency_rates(modeladmin, request, queryset):
-    backend = currency_Backend()
-    backend.refresh_rate()
-    messages.success(request, "Currency rates refreshed successfully.")
+
+
 @admin.register(CurrencyExchange)
-class CurrencyExchangeAdmin(admin.ModelAdmin): # TODO: Distable refresh buttion when user not in DEBUG mode
+class CurrencyExchangeAdmin(admin.ModelAdmin):
     list_display = ('base_currency', 'target_currency', 'humanize_rate', 'created_at', 'last_modified')
-    actions = [refresh_currency_rates]
+    change_list_template = "admin/currency_exchange_change_list.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('refresh-rates/', self.admin_site.admin_view(self.refresh_rates), name='refresh-rates'),
+        ]
+        return custom_urls + urls
+
+    def refresh_rates(self, request):
+        if settings.DEBUG:  # Only allow refresh in DEBUG mode
+            backend = currency_Backend()
+            backend.refresh_rate()
+            messages.success(request, "Currency rates refreshed successfully.")
+        else:
+            messages.error(request, "Currency rates refresh is disabled in production.")
+        return HttpResponseRedirect("../")
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['debug'] = settings.DEBUG
+        return super(CurrencyExchangeAdmin, self).changelist_view(request, extra_context=extra_context)
