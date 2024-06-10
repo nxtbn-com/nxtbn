d49a9d23ba582fa049ba5598f9925b0627367161
MySecondLanguage <bytenyxlimited@gmail.com>
2024-06-10 13:41:54 +0600

    Implemented login

--- nxtbn/users/api/dashboard/urls.py
+++ nxtbn/users/api/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from nxtbn.users.api.dashboard import views as users_views
 
 urlpatterns = [
-    
+    path('login/', users_views.LoginView.as_view(), name='login'),
 ]
--- nxtbn/users/api/dashboard/views.py
+++ nxtbn/users/api/dashboard/views.py
@@ -3,3 +3,42 @@ from rest_framework.response import Response
 from django.utils.translation import gettext_lazy as _
 from rest_framework.permissions  import AllowAny
 from rest_framework.exceptions import APIException
+from django.contrib.auth import authenticate
+from allauth.account.models import EmailAddress
+
+
+from nxtbn.users.api.dashboard.serializers import DashboardLoginSerializer
+from nxtbn.users.api.storefront.serializers import JwtBasicUserSerializer
+from nxtbn.users.utils.jwt_utils import generate_access_token, generate_refresh_token
+
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = DashboardLoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(email=email, password=password)
+        if user:
+            access_token = generate_access_token(user)
+            refresh_token = generate_refresh_token(user)
+
+            user_data = JwtBasicUserSerializer(user).data
+            return Response(
+                {
+                    "user": user_data,
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+
+        return Response({"detail": _("Invalid credentials")}, status=status.HTTP_400_BAD_REQUEST)
