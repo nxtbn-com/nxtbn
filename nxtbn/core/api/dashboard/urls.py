90e1f3da28e2ea7396f7ae30848c67acd6277c18
MySecondLanguage <bytenyxlimited@gmail.com>
2024-06-14 22:08:52 +0600

    Refactored file structure

--- nxtbn/core/api/dashboard/urls.py
+++ nxtbn/core/api/dashboard/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from nxtbn.core.api.dashboard import views as core_views
 
 urlpatterns = [
-    path('pluggin-install-via-git/', core_views.PlugginsInstallViaGitView.as_view(), name='upload_pluggins_via_git'),
-    path('upload-pluggins/', core_views.PlugginsUploadView.as_view(), name='upload_pluggins'),
-    
+        
 ]
---
+++ nxtbn/plugins/api/dashboard/serializers.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+import os
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+from django.db import transaction
+
+from nxtbn.plugins import PluginType
+
+class ZipFileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
+    plugin_type = serializers.ChoiceField(choices=PluginType.choices)
+    
+    def validate_file(self, value):
+        valid_extensions = ['.zip',]
+        ext = os.path.splitext(value.name)[1].lower()
+        
+        if ext not in valid_extensions:
+            raise serializers.ValidationError(f"Invalid file extension: {ext}.")
+        
+        if value.size > 5 * 1024 * 1024:  # 5 MB
+            raise serializers.ValidationError("File is too large. Maximum size is 5 MB.")
+
+        return value
+
+
+class PluginInstallSerializer(serializers.Serializer):
+    git_url = serializers.URLField()
+    plugin_type = serializers.ChoiceField(choices=PluginType.choices)
+
+
+class PluginInstallWithZIPURLSerializer(serializers.Serializer):
+    zip_url = serializers.URLField()
+    plugin_type = serializers.ChoiceField(choices=PluginType.choices)
